c3fde5446d66c10a0f376251e11466ca8c5d1b35
Lokeshnrm <104585148+Lokeshnrm@users.noreply.github.com>
2025-01-11 19:06:44 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Chatbot from "./Chatbot";
+import "./styles/Chatbot.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>CDP Support Chatbot</h1>
+            <Chatbot />
+        </div>
+    );
+}
+
+export default App;
